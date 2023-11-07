db7c8b8b78e8ff3b49abe7d3908d2d5aa3336c9f
himangshu147-git <saikiahimangshu1256@gmail.com>
2023-11-07 19:52:00 +0530

    new minor chnage

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ requirements = [
 
 setup(
     name='bhaicord.py',
-    version='0.0.1',
+    version='0.0.2',
     author='Your Name',
     author_email='147.himangshu@gmail.com',
     description='A discord API wrapper for python',
